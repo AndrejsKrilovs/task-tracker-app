483213afbd505695a48064e1edc10676470a5f9d
Andrejs Krilovs <andrejs.krilovs.0507@gmil.com>
2025-06-07 14:32:04 +0300

    Configured typescript

---
+++ src/main/webui/src/counter.ts
@@ -0,0 +1,11 @@
+export function setupCounter(element: HTMLButtonElement): void {
+  let counter = 0
+
+  const setCounter = (count: number) => {
+    counter = count
+    element.innerText = `count is ${counter}`
+  }
+
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
---
+++ src/main/webui/vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+// В простом TypeScript проекте без фреймворков — никаких плагинов не нужно
+export default defineConfig({
+  root: '.', // если index.html находится в корне проекта (по умолчанию)
+})
