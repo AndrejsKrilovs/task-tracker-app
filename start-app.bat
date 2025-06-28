b54300365f0d7f842db9675e4ffc80a993ebe2eb
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2025-06-29 01:22:27 +0300

    Application can run locally from docker-hub image

--- start-app.bat
+++ start-app.bat
@@ -2,27 +2,11 @@
 setlocal EnableDelayedExpansion
 
 set "COMPOSE_FILE=.\docker\docker-compose.yaml"
-set "IMAGE_BASE_NAME=task-tracker-app"
-set "APP_VERSION="
 
-:: Извлечь версию из docker-compose.yaml
-for /f "usebackq tokens=1,* delims=:" %%A in (`findstr /R /C:"APP_VERSION:" "%COMPOSE_FILE%"`) do (
-    set "APP_VERSION=%%B"
-    set "APP_VERSION=!APP_VERSION: =!"
-)
+echo Pulling image from docker-hub...
+docker pull andrejskrilovs/task-tracker-app:4.0
 
-echo Detected APP_VERSION: %APP_VERSION%
-echo Cleaning and building project...
-call .\gradlew clean build
-
-echo Building and starting containers with version %APP_VERSION%...
-docker-compose --env-file .env -f "%COMPOSE_FILE%" up --build -d
-
-:: Удаление dangling образов (с тегом <none>)
-echo Removing dangling images...
-for /f %%I in ('docker images -f "dangling=true" -q') do (
-    echo Removing dangling image: %%I
-    docker rmi -f %%I
-)
+echo Starting application...
+docker-compose --env-file .env -f "%COMPOSE_FILE%" up -d
 
 endlocal
