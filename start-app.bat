f07756b34d23b774ed9d32da80919dc06eb06072
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2025-12-12 14:29:04 +0200

    Changing application run od dev environment

--- start-app.bat
+++ start-app.bat
@@ -7,6 +7,6 @@ echo Pulling image from docker-hub...
 docker pull andrejskrilovs/task-tracker-app:4.0
 
 echo Starting application...
-docker-compose --env-file .env -f "%COMPOSE_FILE%" up -d
+docker-compose --env-file .env -f "%COMPOSE_FILE%" up
 
 endlocal
--- start-app.sh
+++ start-app.sh
@@ -8,4 +8,4 @@ echo "Pulling image from docker-hub..."
 docker pull andrejskrilovs/task-tracker-app:4.0
 
 echo "Starting application..."
-docker-compose --env-file .env -f "$COMPOSE_FILE" up -d
+docker-compose --env-file .env -f "$COMPOSE_FILE" up
